89bc47f685b51fc89f282d26968a607a7707b858
Tamar Kalir <tkalir@gmail.com>
2025-07-24 21:16:49 +0300

    adding skip_generation option to generate-and-send-infographics-images

--- airflow_server/dags/generate_and_send_infographics_images.py
+++ airflow_server/dags/generate_and_send_infographics_images.py
@@ -1,4 +1,6 @@
 from airflow import DAG
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.operators.python import BranchPythonOperator
 import pendulum
 from anyway_etl_airflow.operators.cli_bash_operator import CliBashOperator
 
@@ -10,24 +12,45 @@ dag_kwargs = dict(
     start_date=pendulum.datetime(2023, 4, 1, tz="Asia/Jerusalem")
 )
 
-with DAG('generate-and-send-infographics-images', **dag_kwargs, schedule_interval=None,
+DAG_ID = "generate-and-send-infographics-images"
+GENERATE_INFOGRAPHICS_TASK_ID = 'generate-infographics-images'
+UPLOAD_INFOGRAPHICS_TASK_ID = 'upload-infographics-images'
+SEND_INFOGRAPHICS_TASK_ID = 'send-infographics-to-telegram'
+
+def decide_branch(**context):
+    skip = context["dag_run"].conf.get("skip_generation", False)
+    return SEND_INFOGRAPHICS_TASK_ID if skip else GENERATE_INFOGRAPHICS_TASK_ID
+
+with DAG(DAG_ID, **dag_kwargs, schedule_interval=None,
          description='Generates infographics for newsflash id and sends images to Telegram. '
                      'Must run manually with json, example:'
                      '{"news_flash_id": "65516"}') as generate_and_send_infographics_images_dag:
-    CliBashOperator(
+
+    branch = BranchPythonOperator(
+        task_id="check_skip_generation",
+        python_callable=decide_branch,
+    )
+
+    generate_infographics_images  = CliBashOperator(
         cmd='anyway-etl anyway-kubectl-exec python3 main.py '
             'process infographics-pictures --id {{ dag_run.conf["news_flash_id"] }}',
-        task_id='generate-infographics-images',
+        task_id=GENERATE_INFOGRAPHICS_TASK_ID,
         retries=8
-    ) >> \
-    CliBashOperator(
+    )
+
+    upload_infographics_images = CliBashOperator(
         cmd='anyway-etl anyway-kubectl-exec python3 main.py '
             'upload generated-infographics --id {{ dag_run.conf["news_flash_id"] }}',
-        task_id='upload-infographics-images'
-    ) >> \
-    CliBashOperator(
+        task_id=UPLOAD_INFOGRAPHICS_TASK_ID
+    )
+
+    send_infographics_to_telegram  = CliBashOperator(
         cmd='anyway-etl anyway-kubectl-exec python3 main.py '
             'telegram send-notification --id {{ dag_run.conf["news_flash_id"] }} '
             '--chat {{ dag_run.conf["chat_id"] }}',
-        task_id='send-infographics-to-telegram'
+        task_id=SEND_INFOGRAPHICS_TASK_ID,
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS,
     )
+
+    branch >> generate_infographics_images >> upload_infographics_images >> send_infographics_to_telegram
+    branch >> send_infographics_to_telegram
